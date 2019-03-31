f128e879a0e8152b486cfc9516e177583e71ddde
Tore Sinding Bekkedal <toresbe@gmail.com>
2019-03-31 21:12:14 +0200

    add unit test coverage to pysykkel and setuptools config file

--- pysykkel/__init__.py
+++ pysykkel/__init__.py
@@ -1,4 +1,5 @@
 import requests
+from twisted.trial import unittest
 
 class PysykkelError(Exception):
     pass
@@ -9,10 +10,23 @@ class PysykkelHTTPError(PysykkelError):
 class PysykkelJSONError(PysykkelError):
     pass
 
+class PysykkelTest(unittest.TestCase):
+    def test_raises_json_error_on_invalid_json(self):
+        p = Pysykkel()
+        p.base_url = 'http://google.com/?'
+        with self.assertRaises(PysykkelJSONError):
+            stations = p.stations()
+
+    def test_raises_http_error_on_invalid_url(self):
+        p = Pysykkel()
+        p.base_url = 'http://this is certainly not a valid url'
+        with self.assertRaises(PysykkelHTTPError):
+            stations = p.stations()
+
 class Pysykkel:
     base_url = 'https://gbfs.urbansharing.com/oslobysykkel.no'
 
-    def __init__(self, client_id = None):
+    def __init__(self, client_id = 'pysykkel-development'):
         self.headers = {'Client-Identifier': client_id}
 
     def _get_json(self, location):
@@ -25,7 +39,7 @@ class Pysykkel:
             return data.json()
         except ValueError:
             raise PysykkelJSONError('Received invalid JSON from Bysykkel API while trying to fetch ' + location)
-        except requests.exceptions.HTTPError as e:
+        except requests.exceptions.RequestException as e:
             raise PysykkelHTTPError('Got HTTP error while requesting from Bysykkel API (' + location + '): ' + str(e))
 
     @property
@@ -41,5 +55,6 @@ class Pysykkel:
         # is not accessible, it will simply be omitted
         for station in station_data:
             station['availability'] = [item for item in availability_list if item[0] == station['station_id']][0][1]
+            del station['availability']['station_id'] # butter on pork, as we say
 
         return station_data
