e0a9947fa5234b69d98be077e4ace8bd43c4cefa
Tore Sinding Bekkedal <toresbe@gmail.com>
2019-03-31 14:05:49 +0200

    Update code base to use new GBFS API

--- README.md
+++ README.md
@@ -24,7 +24,9 @@ pip install -r requirements.txt
 
 ## Usage
 
-Place the API key in a text file called api-key.txt in your working directory.
+The API requests that the Client-Identifier header be supplied, with the organization name, a dash, followed by the application name, eg. 'acme-publicdisplay'.
+
+As shipped the client-identifier is 'pysykkel-development'; if you intend to use this library to generate a lot of requests, please change this for everyone's benefit.
 
 If everything went smoothly, you should now be able to run the test case:
 
--- pysykkel/__init__.py
+++ pysykkel/__init__.py
@@ -11,24 +11,21 @@ class PysykkelJSONError(Exception):
     pass
 
 class Pysykkel:
-    base_url = 'https://oslobysykkel.no/api/v1'
+    base_url = 'https://gbfs.urbansharing.com/oslobysykkel.no'
 
     def __init__(self):
         try:
-            with open('api-key.txt', 'r') as api_key_file:
-                api_key = api_key_file.read().strip()
-                assert len(api_key) == 32
+            with open('client-identifier.txt', 'r') as client_id_file:
+                client_id = client_id_file.read().strip()
         except OSError as e:
-            raise OSError('Could not open API key file: ' + str(e))
-        except AssertionError:
-            print('Unexpected length of API key, expected 32 bytes')
+            raise OSError('Could not open client ID file: ' + str(e))
 
-        self.headers = {'Client-Identifier': api_key}
+        self.headers = {'Client-Identifier': client_id}
 
-    ### Obtain the JSON data of GET on base_url + location as a dictionary.
-    ### Raises PysykkelHTTPError if the HTTP request fails,
-    ### Raises PysykkelJSONError if the JSON data could not be decoded
     def _get_json(self, location):
+        """Obtain the JSON data of GET on base_url + location as a dictionary.
+        Raises PysykkelHTTPError if the HTTP request fails,
+        Raises PysykkelJSONError if the JSON data could not be decoded"""
         try:
             data = requests.get(self.base_url + location, headers=self.headers)
             data.raise_for_status()
@@ -38,26 +35,26 @@ class Pysykkel:
         except requests.exceptions.HTTPError as e:
             raise PysykkelHTTPError('Got HTTP error while requesting from Bysykkel API (' + location + '): ' + str(e))
 
-    ### Return a list of all stations in the system, including availability data
     @property
     def stations(self):
-        station_data = self._get_json('/stations')['stations']
-        availability_data = self._get_json('/stations/availability')['stations']
+        """Return a list of all stations in the system, including availability data"""
+        station_data = self._get_json('/station_information.json')['data']['stations']
+        availability_data = self._get_json('/station_status.json')['data']['stations']
 
         # Munge the dict into a list of (id, availability_dict) tuples
-        availability_list = [(a['id'], a['availability']) for a in availability_data]
+        availability_list = [(a['station_id'], a) for a in availability_data]
 
         # Merge availability and station data. If availability data for a given station
         # is not accessible, it will simply be omitted
         for station in station_data:
-            station['availability'] = [item for item in availability_list if item[0] == station['id']][0][1]
+            station['availability'] = [item for item in availability_list if item[0] == station['station_id']][0][1]
 
         return station_data
 
-    ### Return a dict with a list of closed stations, and a boolean value
-    ### saying whether all stations are closed.
     @property
     def status(self):
+        """ Return a dict with a list of closed stations, and a boolean value
+        saying whether all stations are closed."""
         a = requests.get(self.base_url + '/status', headers=self.headers).json()
         return a['status']
 
